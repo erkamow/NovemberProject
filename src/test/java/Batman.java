e9acc467b13959177d3b20a144747fcf1ba0fa61
erkamow <erkamafsin@afsteknik.com>
2020-11-03 17:55:10 +0300

    some changes for Batman class

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -1,5 +1,8 @@
 public class Batman {
     public static void main(String[] args) {
         System.out.println("batman");
+        // yes he will catch him
+        // batman has cool bat mobile
+        // batman has cool bat motorcycle
     }
 }
